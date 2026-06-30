dc16b0aa329b04c343bee916c166b30435f0bdba
Eky <eky@Eky-MacBook-Air.local>
2026-06-30 10:51:09 +0300

    Add Eky test endpoint

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ app = FastAPI() # FastAPI application instance
 @app.get("/") # endpoint for the root URL. Kullanıcı / adresine get isteği yaptığında home fonksiyonu çalıştırılır.
 def home():
     return {"message": "Welcome to the FastAPI application!"}
+
+
+@app.get("/eky")
+def bu_test_fonksiyonu():
+    return {
+        "deneme": "bu test fonksiyonu çalıştı",
+        "deneme2": "bu test fonksiyonu çalıştı 2",
+        "sonuc": "200",
+        "bir de array": ["deneme1", "deneme2", "deneme3"]
+    }
